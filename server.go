dcfc6ea7786a9c2c1a74f2c1d42ca9fbf76f2b29
vllasa723-ctrl <vllasa723@gmail.com>
2026-05-05 17:42:10 +0300

    Update server.go

--- server.go
+++ server.go
@@ -1,11 +1,22 @@
 package main
 
-import "net/http"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		w.Write([]byte("ok"))
 	})
-	go http.ListenAndServe("127.0.0.1:8080", nil)
-	http.ListenAndServeTLS("127.0.0.1:8443", "cert.pem", "key.pem", nil)
+	
+	go func() {
+		err := http.ListenAndServe("127.0.0.1:8080", nil)
+		if err != nil {
+			log.Printf("HTTP error: %v", err)
+		}
+	}()
+	
+	// Оберни в log.Fatal, чтобы видеть причину смерти!
+	log.Fatal(http.ListenAndServeTLS("127.0.0.1:8443", "cert.pem", "key.pem", nil))
 }
